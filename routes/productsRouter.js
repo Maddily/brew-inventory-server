aaf07861004ec0c11afeb44941457647ccbc3bb4
Maddily <115709272+Maddily@users.noreply.github.com>
2026-06-26 12:19:22 +0300

    Create products router

---
+++ routes/productsRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  getProducts,
+  getProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productsController.js");
+
+const productsRouter = express.Router();
+
+productsRouter.get("/", getProducts);
+productsRouter.get("/:id", getProduct);
+productsRouter.post("/", addProduct);
+productsRouter.put("/:id", updateProduct);
+productsRouter.delete("/:id", deleteProduct);
+
+module.exports = productsRouter;
