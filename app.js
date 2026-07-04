690fb1eda4b0e432714f18bdc350ae38525a313f
Maddily <115709272+Maddily@users.noreply.github.com>
2026-07-04 12:09:18 +0300

    Use cors middleware

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const categoriesRouter = require("./routes/categoriesRouter.js");
 const productsRouter = require("./routes/productsRouter.js");
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
 
 app.use("/api/categories", categoriesRouter);
 app.use("/api/products", productsRouter);
